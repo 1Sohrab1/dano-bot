cd073f3efa54e9a61c74e14a9b97087d0d12241b
Sohrab <1alifaramarzi1@gmail.com>
2026-08-16 15:28:05 +0330

    refactor: move admin authorization to filter

---
+++ app/filters/admin.py
@@ -0,0 +1,12 @@
+from aiogram.filters import BaseFilter
+from aiogram.types import Message
+
+from app.services.admin_service import is_admin
+
+
+class AdminFilter(BaseFilter):
+    async def __call__(self, message: Message) -> bool:
+        if message.from_user is None:
+            return False
+
+        return is_admin(message.from_user.id)
