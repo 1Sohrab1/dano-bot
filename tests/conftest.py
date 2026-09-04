80deaab02410bc53c6ee9682bccadaed039571d2
mamahoos <m4m4hoos@gmail.com>
2026-09-04 15:05:36 +0330

    test: provide isolated template test settings

---
+++ tests/conftest.py
@@ -0,0 +1,7 @@
+import os
+
+
+os.environ.setdefault("BOT_TOKEN", "test-token")
+os.environ.setdefault("ADMIN_IDS", "[123456789]")
+os.environ.setdefault("DATABASE_URL", "sqlite+aiosqlite:///./data/test.db")
+os.environ.setdefault("REQUIRED_CHANNEL_ID", "@test-channel")
