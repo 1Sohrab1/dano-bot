36d4cbb819bb4754f05ea9935776ef3a4e927678
mamahoos <m4m4hoos@gmail.com>
2026-09-04 15:02:22 +0330

    fix: remove missing user middleware from template

--- app/router/user/__init__.py
+++ app/router/user/__init__.py
@@ -1,9 +1,7 @@
 from . import handlers
 from .filters import UserFilter
-from .middleware import UserMiddleware
 from .router import users
 
 users.message.filter(UserFilter())
-users.message.middleware(UserMiddleware())
 
 __all__ = ["users"]
