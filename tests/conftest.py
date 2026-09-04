e73e5aae1d325254c17c6bccaf5716e4defad841
mamahoos <m4m4hoos@gmail.com>
2026-09-04 15:06:41 +0330

    style: format test environment fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,5 @@
 import os
 
-
 os.environ.setdefault("BOT_TOKEN", "test-token")
 os.environ.setdefault("ADMIN_IDS", "[123456789]")
 os.environ.setdefault("DATABASE_URL", "sqlite+aiosqlite:///./data/test.db")
