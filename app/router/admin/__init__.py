b48536f034504b5d0927ad690f3a36e01af3c94b
mamahoos <m4m4hoos@gmail.com>
2026-09-04 15:02:08 +0330

    fix: remove missing admin middleware from template

--- app/router/admin/__init__.py
+++ app/router/admin/__init__.py
@@ -1,9 +1,7 @@
 from . import handlers
 from .filters import AdminFilter
-from .middleware import AdminMiddleware
 from .router import admins
 
 admins.message.filter(AdminFilter())
-admins.message.middleware(AdminMiddleware())
 
 __all__ = ["admins"]
